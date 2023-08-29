76da0691fe0156a48a3b786bf34360e55c161083
lesenelir <hi@lesenelir.me>
2023-08-29 18:39:47 +0800

    chore: change file name

---
+++ src/source/main.ts
@@ -0,0 +1,10 @@
+export const textList: string[] = [
+  "Let's go",
+  "Let's design",
+  "Let's build",
+  "Let's discover",
+  "Let's explore",
+  "Let's innovate",
+  "Let's chit-chat",
+  "Let's brainstorm",
+]
