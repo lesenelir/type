cc17729b907d309752193c8d28a88ead6a0a3389
lesenelir <hi@lesenelir.me>
2023-08-29 18:43:44 +0800

    feat: implement typewriter effect as seen on OpenAI website

--- src/pages/openai.tsx
+++ src/pages/openai.tsx
@@ -1,27 +1,12 @@
-import {INavbarLeft, INavbarRight} from "@/types/data.ts"
-import {navbarLeftData, navbarRightData} from "@/source/navbar.tsx"
+import Typewriter, {TypewriterClass} from 'typewriter-effect'
 
+import {INavbarLeft, INavbarRight} from "@/types/data.ts"
+import {navbarLeftData, navbarRightData} from "@/source/navbar.ts"
 import LZIcon from "@/components/icons/LZIcon.tsx"
 import NavbarLi from "@/components/openai/NavbarLi.tsx"
+import ArrowUpRightIcon from "@/components/icons/ArrowUpRightIcon.tsx"
 
-// const textList: string[] = [
-//   "Let's go",
-//   "Let's design",
-//   "Let's build",
-//   "Let's discover",
-//   "Let's explore",
-//   "Let's innovate",
-//   "Let's chit-chat",
-//   "Let's brainstorm",
-// ]
-
-// const navbarList: string[] = [
-//   'Research',
-//   'API',
-//   'ChatGPT',
-//   'Safety',
-//   'Company',
-// ]
+import '@/style/openai.css'
 
 function OpenAIPage() {
   return (
@@ -50,13 +35,51 @@ function OpenAIPage() {
       </nav>
 
       {/* Content */}
-      <main>
-
+      <main className={'flex-1 flex flex-col justify-center items-center p-5'}>
+        <Typewriter
+          options={{
+            delay: 10,
+            cursor: '',
+          }}
+          onInit={(typewriter: TypewriterClass) => {
+            typewriter
+              .pauseFor(3000)
+              .typeString('hello world')
+              .start()
+          }}
+        />
       </main>
 
       {/* Footer */}
-      <footer>
-
+      <footer className={'p-5 pb-16'}>
+        <Typewriter
+          options={{
+            delay: 10,
+            cursor: '',
+            cursorClassName: 'Typewriter__cursor--circle',
+          }}
+          onInit={(typewriter: TypewriterClass) => {
+            typewriter
+              .typeString(`<span class="text-lg">Lesenelir AI Base: </span>`)
+              .typeString(`<span>A collection of art and code works about AI, inspired by OpenAI.</span>`)
+              .typeString('<br/>')
+              .typeString(`<span>This project includes: ChatGPT and DALL-E.</span>`)
+              .callFunction(() => {
+                // hidden cursor when typing complete
+                const cursorElement = document.querySelector('.Typewriter__cursor--circle')
+                cursorElement && cursorElement.remove()
+              })
+              .start()
+          }}
+        />
+        <div className={'flex flex-row'}>
+          <p className={'mt-4 underline underline-offset-4 cursor-pointer hover:text-opacity-30'}>
+            <a href={'https://github.com/lesenelir'} target={'_blank'} rel={'noopener noreferrer'}>
+              Find the author
+            </a>
+          </p>
+          <ArrowUpRightIcon width={24} height={24} className={'ml-1 mt-4'}/>
+        </div>
       </footer>
     </div>
   )
